40c482229b8a4915072b4cd2a8f2645701ac29f1
Usman Zaheer <usmanzaheer1995@gmail.com>
2018-05-05 18:37:12 +0500

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,59 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  // loaders
-  module: {
-    rules: [
-      {
-        // this rule says that whenever you see a file ending with .js and it's not in
-        // the node_modules folder, go ahead and run it through babel
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        // the ? in s?css means that the s is optional
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
-  },
-  // devtool is used for debugging
-  devtool: 'cheap-module-source-map',
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
+    },
+    // loaders
+    module: {
+      rules: [
+        {
+          // this rule says that whenever you see a file ending with .js and it's not in
+          // the node_modules folder, go ahead and run it through babel
 
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          // the ? in s?css means that the s is optional
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              }, 
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }
+      ]
+    },
+    plugins: [
+      CSSExtract
+    ],
+
+    // devtool is used for debugging
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true
+    }
   }
 };
